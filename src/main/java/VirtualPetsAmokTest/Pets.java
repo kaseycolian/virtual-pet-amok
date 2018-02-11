997a8fdfaa1adfcb8731d15d4268d4e423c3f0ac
Kasey <KaseyColian@gmail.com>
2018-02-10 20:41:39 -0500

    Created Pets Abstract Superclass. Also fixed FireLevel variable that didn't refactor into MagicLevel

---
+++ src/main/java/VirtualPetsAmokTest/Pets.java
@@ -0,0 +1,51 @@
+package VirtualPetsAmokTest;
+
+public abstract class Pets {
+
+	protected String microchipNumber;
+	protected String nameOfPet;
+	protected String typeOfPet;
+	static final int DEFAULT_BOREDOMLEVEL = 10;
+	static final int DEFAULT_HAPPYLEVEL = 50;
+	static final int DEFAULT_HEALTHLEVEL = 80;
+	static final int DEFAULT_MAGICLEVEL = 50;
+	protected int healthLevel = DEFAULT_HEALTHLEVEL;
+	protected int happyLevel = DEFAULT_HAPPYLEVEL;
+	protected int magicLevel = DEFAULT_MAGICLEVEL;
+	protected int boredomLevel = DEFAULT_BOREDOMLEVEL;
+
+	public Pets() {
+		super();
+	}
+
+	public String getPetName() {
+
+		return nameOfPet;
+	}
+
+	public String getTypeOfPet() {
+
+		return typeOfPet;
+	}
+
+	public String getMicrochipNumber() {
+		return microchipNumber;
+	}
+
+	public int getBoredomLevel() {
+		return boredomLevel;
+	}
+
+	public int getMagicLevel() {
+		return magicLevel;
+	}
+
+	public int getHappyLevel() {
+		return happyLevel;
+	}
+
+	public int getHealthLevel() {
+		return healthLevel;
+	}
+
+}
